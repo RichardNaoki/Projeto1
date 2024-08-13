6184f867fe5106fe54bf59a5ba3c7c8752f4dca3
richard <richardnaoki@hotmail.com>
2024-08-13 07:30:20 -0300

    AttProjeto

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true
+    },
+  },
+  
+  
+  plugins: [],
+}
+
+// module.exports={
+//   darkMode: 'class',
+// theme:{
+//   extend:{}
+// },
+// variants: {
+//   extend: {},
+// },
+// plugins:[],
+// }
